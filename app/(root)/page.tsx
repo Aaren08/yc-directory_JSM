0b9b0f363f4ba17cc3e9326b1f058929a0a06cb6
Aaren08 <schneiderneil392@gmail.com>
2025-10-18 09:16:08 +0500

    Startup Card setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "../../components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({
   searchParams,
@@ -6,6 +7,20 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 50,
+      author: { _id: 1, name: "John Doe" },
+      _id: "1",
+      description: "Hi there",
+      image:
+        "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+      category: "Software",
+      title: "Mechanic",
+    },
+  ];
   return (
     <>
       <section className="pink_container">
@@ -18,6 +33,24 @@ export default async function Home({
           Competitions.
         </p>
         <SearchForm query={query} />
+      </section>
+
+      {/* ALL STARTUPS & STARTUP CARD */}
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found!</p>
+          )}
+        </ul>
       </section>
     </>
   );
---
+++ components/StartupCard.tsx
@@ -0,0 +1,81 @@
+import { formatDate } from "@/lib/utils";
+import { EyeIcon, ChevronRight } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+const StartupCard = ({ post }: { post: StartupTypeCard }) => {
+  const {
+    _createdAt,
+    views,
+    author: { _id: authorId, name },
+    _id,
+    description,
+    image,
+    category,
+    title,
+  } = post;
+  return (
+    <li className="startup-card group">
+      <div className="flex-between">
+        <p className="startup_card_date">{formatDate(_createdAt)}</p>
+        {/* VIEWS */}
+        <div className="flex gap-1.5">
+          <EyeIcon className="size-6 text-primary" />
+          <span className="text-16-medium">{views}</span>
+        </div>
+      </div>
+
+      {/* NAME & TITLE */}
+      <div className="flex-between mt-5 gap-5">
+        <div className="flex-1">
+          <Link href={`/user/${authorId}`}>
+            <p className="text-16-medium line-clamp-1">{name}</p>
+          </Link>
+          <Link href={`/startup/${_id}`}>
+            <h3 className="text-26-semibold line-clamp-1">{title}</h3>
+          </Link>
+        </div>
+        <Link href={`/user/${authorId}`}>
+          <Image
+            src={"https://placehold.co/600x400"}
+            alt={"placeholder"}
+            width={48}
+            height={48}
+            className="rounded-full"
+          />
+        </Link>
+      </div>
+
+      {/* STARTUP DETAILS */}
+      <Link href={`/startup/${_id}`}>
+        <p className="startup-card_desc">{description}</p>
+        <Image
+          src={image}
+          alt={"placeholder"}
+          width={500}
+          height={164}
+          className="startup-card_img"
+        />
+      </Link>
+
+      {/* CARD FOOTER */}
+      <div className="flex-between gap-3 mt-5">
+        <Link href={`/?query=${category.toLowerCase()}`}>
+          <p className="text-16-medium">{category}</p>
+        </Link>
+        <Button
+          className="startup-card_btn group/details transform transition-all duration-300 hover:scale-[1.03]"
+          asChild
+        >
+          <Link href={`/startup/${_id}`} className="flex items-center gap-1">
+            Details
+            <ChevronRight className="-mx-1.5 transition-transform duration-300 group-hover/details:translate-x-1" />
+          </Link>
+        </Button>
+      </div>
+    </li>
+  );
+};
+
+export default StartupCard;

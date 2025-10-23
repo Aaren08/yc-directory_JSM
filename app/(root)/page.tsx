327ccedf21dbc002a796d477e58120e54e34dce1
Aaren08 <schneiderneil392@gmail.com>
2025-10-23 12:40:32 +0500

    Performance and Bug tracking using Sentry

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,3 @@
-import { auth } from "@/auth";
 import SearchForm from "../../components/SearchForm";
 import StartupCard from "@/components/StartupCard";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
@@ -12,11 +11,6 @@ export default async function Home({
 }) {
   const query = (await searchParams).query;
   const params = { search: query || null };
-
-  const session = await auth();
-  // Debug: log full session then the user id (session.id is undefined because id is stored on session.user)
-  console.log("session:", session);
-  console.log("session.user.id:", session?.user?.id);
 
   const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://ea479f63d7355da7bfd0438e661a4278@o4510237306388480.ingest.de.sentry.io/4510237310451792",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
